1fe4904215de69ad5d1ee05b5e1fc1d35b455470
ludberk <167118384+ludberk@users.noreply.github.com>
2024-05-11 17:14:17 -0700

    update structure

--- backend/config/jwtToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  return jwt.sign({id},process.env.JWT_SECRET_KEY,{expiresIn:"1d"})
-};
-
-module.exports = {generateToken};
--- backend/utils/validateMongoDbId.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const validateMongoDbId = (id) => {
-  const isValid = mongoose.Types.ObjectId(id);
-  if (!isValid) {
-    throw new Error("This id is not a valid or not Found");
-  }
-};
-
-module.exports = validateMongoDbId;
