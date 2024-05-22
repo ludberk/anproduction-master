8f30779e9864b92f61771950f0ef9717aaa0a6b3
Parvin-Gasimli <parvinzga@code.edu.az>
2024-05-22 23:43:22 +0400

    added new

--- client/src/helper/Api.js
+++ client/src/helper/Api.js
@@ -1,35 +1,35 @@
-import axios from "axios";
+import axios from 'axios'
 
 const Http = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com/",
-});
+    baseURL: 'https://jsonplaceholder.typicode.com/',
+})
 
 export const getUserApi = async () => {
-  try {
-    const response = await Http.get("users");
-    if (response) {
-      return response.data;
-    } else {
-      return [];
+    try {
+        const response = await Http.get('users')
+        if (response) {
+            return response.data
+        } else {
+            return []
+        }
+    } catch (err) {
+        console.error('Error fetching user data:', err.message)
+        throw err
     }
-  } catch (err) {
-    console.error("Error fetching user data:", err.message);
-    throw err;
-  }
-};
+}
 
 export const createGalleryImage = async (data) => {
-  try {
-    const response = await Http.post({
-      data: data,
-    });
-    if (response) {
-      return response.data;
-    } else {
-      return [];
+    try {
+        const response = await Http.post({
+            data: data,
+        })
+        if (response) {
+            return response.data
+        } else {
+            return []
+        }
+    } catch (err) {
+        console.error('Error fetching user data:', err.message)
+        throw err
     }
-  } catch (err) {
-    console.error("Error fetching user data:", err.message);
-    throw err;
-  }
-};
+}
